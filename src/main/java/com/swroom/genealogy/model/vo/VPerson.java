22384829dc7c78c3d4624fb729d4bfd3e0fde5ed
jingz <jingzonglei@163.com>
2017-02-07 11:34:05 +0800

    初始化前端VPerson对象的属性

--- src/main/java/com/swroom/genealogy/model/vo/VPerson.java
+++ src/main/java/com/swroom/genealogy/model/vo/VPerson.java
@@ -1,9 +1,14 @@
 package com.swroom.genealogy.model.vo;
 
+import com.swroom.genealogy.common.Constants;
 import com.swroom.genealogy.common.Enums;
 import com.swroom.genealogy.model.po.GenPerson;
 import com.swroom.genealogy.model.po.GenPersonInfo;
+import com.swroom.genealogy.utils.DateUtil;
+import com.swroom.genealogy.utils.NumericUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,28 +27,28 @@ public class VPerson {
     private String selfName; // 号
 
     // 世序相关
-    private Integer ancestralSeq; // 世序
+    private String ancestralSeq; // 世序
     private String ancestralSeqMemo; // 世序备注
     private String branch; // 家族分支
 
     // 基本信息
     private String sex; // 性别
-    private Integer rank; // 排行
+    private String rank; // 排行
     private String personMemo; // 个人说明
-    private Boolean dead; // 是否已故
+    private String dead; // 是否已故
     private String deathDate; // 已故时间
-    private Boolean heir; // 是否存在过继关系
-    private Integer sonNum; // 子数量
-    private Integer daughterNum; // 女数量
+    private String heir; // 是否存在过继关系
+    private String sonNum; // 子数量
+    private String daughterNum; // 女数量
 
     // 关系网
-    private List<String> spouse; // 配偶，介于古时一夫多妻制
-    private Map<String, String> spouseAddress; // 配偶原址
+    private List<String> spouse = new ArrayList<>(); // 配偶，介于古时一夫多妻制
+    private Map<String, String> spouseAddress = new HashMap<>(); // 配偶原址
     private String father; // 父
     private String mother; // 母
     private String heirFather; // 嗣父，存在过继关系才显示
-    private List<String> brothers; // 兄弟姐妹
-    private List<String> children; // 子女
+    private List<String> brothers = new ArrayList<>(); // 兄弟姐妹
+    private List<String> children = new ArrayList<>(); // 子女
 
     // 个人信息
     private String birthday; // 生辰
@@ -66,10 +71,58 @@ public class VPerson {
         this.styleName = this.genPerson.getStyleName();
         this.selfName = this.genPerson.getSelfName();
 
-        this.ancestralSeq = this.genPerson.getAncestralSequence();
+        this.ancestralSeq = NumericUtil.arabicToNumeric(String.valueOf(genPerson.getAncestralSequence())) + "世";
         this.ancestralSeqMemo = this.genPerson.getAncestralSequenceMemo();
-        this.branch = Enums.getEnuName("branch", String.valueOf(this.genPerson.getBranch()));
+        this.branch = Enums.getEnuName(Constants.BRANCH, String.valueOf(this.genPerson.getBranch()));
 
+        this.sex = Enums.getEnuName(Constants.SEX, String.valueOf(this.genPerson.getSex()));
+        // 排行需要判断男女，男：行一、行二；女：女一、女二
+        String rankCN = Enums.getEnuName(Constants.RANK, String.valueOf(this.genPerson.getRank()));
+        String prefix = this.genPerson.getSex() == 1 ? "行" : "女";
+        this.rank = prefix + rankCN;
 
+        personMemo = this.genPerson.getPersonMemo();
+        this.dead = Enums.getEnuName(Constants.BOOLEAN, String.valueOf(this.genPerson.getDead()));
+        this.deathDate = DateUtil.getDateStr(genPerson.getDeathDate());
+        this.heir = Enums.getEnuName(Constants.BOOLEAN, String.valueOf(this.genPerson.getHeir()));
+
+        // 古时一夫多妻，数据库中用`/`做分割
+        String[] spouses = this.genPerson.getSpouseName().split("/");
+        for (int i = 0; i < spouses.length; i++) {
+            this.spouse.add(spouses[i]);
+        }
+        // 配偶地址在数据库中采用 `配偶名一:地址一/配偶名二:地址二/配偶名三:地址三` 形式存储
+        String[] saddrs = this.genPerson.getSpouseAddress().split("/");
+        for (int i = 0; i < saddrs.length; i++) {
+            String[] p = saddrs[i].split(":");
+            this.spouseAddress.put(p[0], p[1]);
+        }
+
+        this.father = this.genPerson.getFatherName();
+        this.mother = this.genPerson.getMotherName();
+        this.heirFather = this.genPerson.getHeirFatherName();
+
+        this.birthday = DateUtil.getDateStr(this.genPersonInfo.getBirthday());
+        this.currentAddress = this.genPersonInfo.getCurrentAddress();
+        this.cellphone = this.genPersonInfo.getCellphone();
+        this.profession = this.genPersonInfo.getProfession();
+        this.position = this.genPersonInfo.getPosition();
+        this.introduction = this.genPersonInfo.getIntroduction();
+    }
+
+    public void setSonNum(String sonNum) {
+        this.sonNum = sonNum;
+    }
+
+    public void setDaughterNum(String daughterNum) {
+        this.daughterNum = daughterNum;
+    }
+
+    public void setBrothers(List<String> brothers) {
+        this.brothers = brothers;
+    }
+
+    public void setChildren(List<String> children) {
+        this.children = children;
     }
 }
