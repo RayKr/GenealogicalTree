06f5cdefe1217d271fa3ddffd156bca92154678c
jingz <jingzonglei@163.com>
2017-02-07 10:30:49 +0800

    增加一个常量类，用于定义枚举类型

---
+++ src/main/java/com/swroom/genealogy/common/Constants.java
@@ -0,0 +1,13 @@
+package com.swroom.genealogy.common;
+
+/**
+ * 常量类
+ * Created by jingz on 2017/2/7.
+ */
+public class Constants {
+
+    public static final String BRANCH = "branch";   // 分支
+    public static final String SEX = "sex";         // 性别
+    public static final String RANK = "rank";       // 排行
+
+}
